59003237c32b7cccdc243a090adb7d698cf978ef
julianzoller <julian.zoller@design-harmony.de>
2020-06-26 13:20:30 +0200

    update gallerie

--- src/views/Socialmediaimg.js
+++ src/views/Socialmediaimg.js
@@ -1,39 +1,136 @@
 const media = [
     
-    {
-      thumb: '/img/135_4.9dabcef2.jpg',
-      src: '/img/135_4.9dabcef2.jpg',
-      caption: '<h4>Elephant</h4>',
-    },
-    {
-      thumb: '/img/DSC00013-31.a263916c.jpg',
-      src: '/img/DSC00013-31.a263916c.jpg',     
-    },
-    {
-      thumb: '/img/Savour.def6a051.jpg',
-      src: '/img/Savour.def6a051.jpg',
-      
-    },
-    {
-      thumb: '/img/final22.654f5ee0.jpg',
-      src: '/img/final22.654f5ee0.jpg',
-     
-    },
-    {
-      thumb: '/img/OchaOchaTee.4916ec4c.jpg',
-      src: '/img/OchaOchaTee.4916ec4c.jpg',
-      
-    },
-    {
-      thumb: '/img/Slider für Anwendungs-Montage (nacher(2).b0c0c9ad.jpg',
-      src: '/img/Slider für Anwendungs-Montage (nacher(2).b0c0c9ad.jpg',
-     
-    },
-    {
-      thumb: '/img/final2.1250a504.jpg',
-      src: '/img/final2.1250a504.jpg',
+  {
+    thumb: '/img/1 Startfoto.43af1340.jpg',  
+    src:'/img/1 Startfoto.43af1340.jpg',
+  },
+  {
+    thumb: '/img/4 Maße.4533095e.jpg',
+    src: '/img/4 Maße.4533095e.jpg',
+    caption: '<h4>Elephant</h4>',
+  },
+  {
+    thumb: '/img/2Maße1.f64aec67.jpg',
+    src: '/img/2Maße1.f64aec67.jpg',     
+  },
+  {
+    thumb: '/img/2USPS.78f8ff66.jpg',
+    src: '/img/2USPS.78f8ff66.jpg',
     
-    },
+  },
+  {
+    thumb: '/img/5Anwendung 1.28185922.jpg',
+    src: '/img/5Anwendung 1.28185922.jpg',
+   
+  },
+  {
+    thumb: '/img/gut.acb12231.jpg',
+    src: '/img/gut.acb12231.jpg',
+    
+  },
+  {
+    thumb: '/img/Foto1_Alkoholmeter_button_neu.c53fea6c.jpg',
+    src: '/img/Foto1_Alkoholmeter_button_neu.c53fea6c.jpg',
+   
+  },
+  {
+    thumb: '/img/Anwendung 1.41ad3d49.jpg',
+    src: '/img/Anwendung 1.41ad3d49.jpg',
+  
+  },
+  {
+    thumb: '/img/5Anwendung1.08c46467.jpg',
+    src: '/img/5Anwendung1.08c46467.jpg',
+   
+  },
+  {
+    thumb: '/img/6 Anwendungsbild2.c067c04d.jpg',
+    src: '/img/6 Anwendungsbild2.c067c04d.jpg',
+  
+  },
+  {
+    thumb: '/img/2Merkmale.4dc415d7.jpg',
+    src: '/img/2Merkmale.4dc415d7.jpg',
+   
+  },
+  {
+    thumb: '/img/Anwendung2.9cca2fff.jpg',
+    src: '/img/Anwendung2.9cca2fff.jpg',
+  
+  },
+  {
+    thumb: '/img/5Montage.be323cd9.jpg',
+    src: '/img/5Montage.be323cd9.jpg',
+   
+  },
+ 
+  {
+    thumb: '/img/Anwendung1.3b15e847.jpg',
+    src: '/img/Anwendung1.3b15e847.jpg',
+   
+  },
+  {
+    thumb: '/img/7 Bewertungen.c4a9ca05.jpg',
+    src: '/img/7 Bewertungen.c4a9ca05.jpg',
+    
+  },
+  {
+    thumb: '/img/4 Lifestyle 80x80.bb123ef0.jpg',
+    src: '/img/4 Lifestyle 80x80.bb123ef0.jpg',
+    
+  },{
+    thumb: '/img/6 Anwendung.3723ef66.jpg',
+    src: '/img/6 Anwendung.3723ef66.jpg',
+    
+  },{
+    thumb: '/img/a+.ddec9da1.jpg',
+    src: '/img/a+.ddec9da1.jpg',
+    
+  },{
+    thumb: '/img/5 Vergleichsbild.1058051b.jpg',
+    src: '/img/5 Vergleichsbild.1058051b.jpg',
+    
+  },{
+    thumb: '/img/4 Lifestyle.77137010.jpg',
+    src: '/img/4 Lifestyle.77137010.jpg',
+    
+  },
+
+  //Socialmedia
+  {
+    thumb: '/img/135_4.9dabcef2.jpg',
+    src: '/img/135_4.9dabcef2.jpg',
+    caption: '<h4>Elephant</h4>',
+  },
+  {
+    thumb: '/img/DSC00013-31.a263916c.jpg',
+    src: '/img/DSC00013-31.a263916c.jpg',     
+  },
+  {
+    thumb: '/img/Savour.def6a051.jpg',
+    src: '/img/Savour.def6a051.jpg',
+    
+  },
+  {
+    thumb: '/img/final22.654f5ee0.jpg',
+    src: '/img/final22.654f5ee0.jpg',
+   
+  },
+  {
+    thumb: '/img/OchaOchaTee.4916ec4c.jpg',
+    src: '/img/OchaOchaTee.4916ec4c.jpg',
+    
+  },
+  {
+    thumb: '/img/Slider für Anwendungs-Montage (nacher(2).b0c0c9ad.jpg',
+    src: '/img/Slider für Anwendungs-Montage (nacher(2).b0c0c9ad.jpg',
+   
+  },
+  {
+    thumb: '/img/final2.1250a504.jpg',
+    src: '/img/final2.1250a504.jpg',
+  
+  },
     
   ]
   
